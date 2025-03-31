0067b7d926a0a5b4970e6b3cd2debacad40b9e8a
Rika <>
2025-03-31 07:06:12 -0400

    Updated package.json.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./**/*.{js,ts,jsx,tsx,mdx}", // Catch all files in the root
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
